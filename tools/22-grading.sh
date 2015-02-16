c6163059debff072b1433768fe6fce3e5ef32f69
Hoa Pham <jmpham@Hoas-MacBook-Air.local>
2015-02-15 23:34:25 -0800

    added back original tests

--- tools/22-grading.sh
+++ tools/22-grading.sh
@@ -17,11 +17,11 @@ sleep 1
 
 ./tools/sbt-peer 11111 ./tools/test-2.torrent ./tools/ SIMPLEBT.TEST.111111 2>/dev/null &
 
-sleep 3
+sleep 1
 
 ./build/simple-bt 60207 ./tools/test-2.torrent > /dev/null 2>&1 &
 
-for ((x = 0; x < 10; x++)); do
+for ((x = 0; x < 30; x++)); do
   printf %s .
   sleep 1
 done
